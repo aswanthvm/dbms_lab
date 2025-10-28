452b9d57bf6b1a030fced21fce948a93da5a24c6
Aswanth V M <150995136+aswanthvm@users.noreply.github.com>
2025-10-28 10:47:42 +0530

    Update practice2.sql

--- practice2.sql
+++ practice2.sql
@@ -148,3 +148,21 @@ COMMIT;
 ------------------------------------------------------------
 -- End of Script
 ------------------------------------------------------------
+fibonaci series::
+DECLARE
+    n NUMBER := 10;
+    a NUMBER := 0;
+    b NUMBER := 1;
+    c NUMBER;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+    DBMS_OUTPUT.PUT_LINE(b);
+    FOR i IN 3..n LOOP
+        c := a + b;
+        DBMS_OUTPUT.PUT_LINE(c);
+        a := b;
+        b := c;
+    END LOOP;
+END;
+/
+
